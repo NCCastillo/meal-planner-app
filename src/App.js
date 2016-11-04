809764f5f28a487248a457cd98476705dc73a7e2
Nestor Castillo <nes_23@yahoo.com>
2016-11-03 21:14:34 -0600

    WIP: adding css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './css/App.css';
+// import './css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Meal Planner App</h1>
+          <button className="button-primary">Button element</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li>Test 2</li>
+          <li>Test 3</li>
+          <li>Test 4</li>
+        </ul>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+// Things I need to do.
+// create components
+//  header
+//  sidebar left
+//  add new meal component (this will be a form to add new meals)
+//  add meals component (this will have a dropdown and a submit/click button)
+//  menu schedule (this will have monday, tues, etc with the meals for those days) 
+//  sign up form
+//  sign in form
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './css/index.css';
+import './css/skeleton.css';
 
 ReactDOM.render(
   <App />,
